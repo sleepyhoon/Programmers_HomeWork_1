07dc0dcb0f0f7b8f38716024bcdc78718ff2a976
sleepyhoon <pokjm99@naver.com>
2025-03-29 20:43:11 +0900

    refactor: view 출력문이 어색하던 문제 해결

--- src/main/java/io/domain/view/OutputView.java
+++ src/main/java/io/domain/view/OutputView.java
@@ -6,17 +6,17 @@ import io.domain.post.dto.ResponsePostDto;
 import java.util.List;
 
 public class OutputView {
-    // 메시지 상수
-    private static final String CREATE_SUCCESS = "번 id로 성공적으로 만들었습니다!";
-    private static final String UPDATE_SUCCESS = "성공적으로 수정했습니다!";
-    private static final String DELETE_SUCCESS = "성공적으로 삭제했습니다!";
-    private static final String SIGNUP_SUCCESS = "번 유저를 성공적으로 회원가입 했습니다!";
+    // 메시지 상수 (String.format용 %d 포함)
+    private static final String CREATE_SUCCESS = "%d번 게시글을 성공적으로 만들었습니다!";
+    private static final String UPDATE_SUCCESS = "%d번 게시글을 성공적으로 수정했습니다!";
+    private static final String DELETE_SUCCESS = "%d번 게시글을 성공적으로 삭제했습니다!";
+    private static final String SIGNUP_SUCCESS = "%d번 유저를 성공적으로 회원가입했습니다!";
     private static final String SIGNIN_SUCCESS = "성공적으로 로그인했습니다!";
     private static final String SIGNIN_FAILURE = "아이디나 비밀번호가 틀렸습니다.";
     private static final String SIGNOUT_SUCCESS = "성공적으로 로그아웃했습니다!";
-    private static final String MEMBER_UPDATE_SUCCESS = "성공적으로 수정했습니다!";
-    private static final String MEMBER_DELETE_SUCCESS = "성공적으로 삭제했습니다!";
-    private static final String UPDATE_START = "번 id를 수정합니다.";
+    private static final String MEMBER_UPDATE_SUCCESS = "회원 정보를 성공적으로 수정했습니다!";
+    private static final String MEMBER_DELETE_SUCCESS = "회원 정보를 성공적으로 삭제했습니다!";
+    private static final String UPDATE_START = "%d번 게시글을 수정합니다.";
     private static final String LOGIN_REQUIRED = "로그인을 먼저 하세요.";
     private static final String INVALID_INPUT = "잘못된 입력입니다.";
     private static final String TERMINATE = "프로그램을 종료합니다.";
@@ -26,11 +26,11 @@ public class OutputView {
         System.out.println(message);
     }
 
-    private static void printWithId(Integer id, String message) {
-        System.out.println(id + message);
+    private static void printWithId(Integer id, String messageTemplate) {
+        System.out.println(String.format(messageTemplate, id));
     }
 
-    // 결과 출력
+    // 게시글 관련 출력
     public static void showCreateResult(Integer postId) {
         printWithId(postId, CREATE_SUCCESS);
     }
@@ -43,6 +43,19 @@ public class OutputView {
         printWithId(postId, DELETE_SUCCESS);
     }
 
+    public static void startUpdate(Integer postId) {
+        printWithId(postId, UPDATE_START);
+    }
+
+    public static void showPost(ResponsePostDto post) {
+        print(post.toString());
+    }
+
+    public static void showAllPosts(List<ResponsePostDto> posts) {
+        posts.forEach(post -> print(post.toString()));
+    }
+
+    // 회원 관련 출력
     public static void showSignUpResult(Integer memberId) {
         printWithId(memberId, SIGNUP_SUCCESS);
     }
@@ -55,20 +68,6 @@ public class OutputView {
         print(SIGNOUT_SUCCESS);
     }
 
-    public static void startUpdate(Integer postId) {
-        printWithId(postId, UPDATE_START);
-    }
-
-    // 게시물 출력
-    public static void showPost(ResponsePostDto post) {
-        print(post.toString());
-    }
-
-    public static void showAllPosts(List<ResponsePostDto> posts) {
-        posts.forEach(post -> print(post.toString()));
-    }
-
-    // 회원 출력
     public static void showMemberDetail(ResponseMemberDetail detail) {
         print(detail.toString());
     }
